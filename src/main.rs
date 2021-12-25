b809d6468dc48eb03ab21814663a669ef6dd613c
William Findlay <william@williamfindlay.com>
2021-12-24 23:03:05 -0500

    project skeleton

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use]
+extern crate rocket;
+
+use rocket::fs::relative;
+use rocket::fs::FileServer;
+use rocket_dyn_templates::Template;
+use serde_json::json;
+
+#[get("/")]
+fn random() -> Template {
+    Template::render("recipe", json!({}))
+}
+
+#[rocket::launch]
+fn rocket() -> _ {
+    rocket::build()
+        .mount("/", FileServer::from(relative!("/static")))
+        .mount("/", routes![random])
+        .attach(Template::fairing())
 }
